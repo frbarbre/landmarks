82d96cf186d58eb1c05e6739d1a486a4937abcb4
Frederik Barbre <fr.barbre@gmail.com>
2025-07-10 00:24:14 +0200

    done with chapter 1, started chapter 2

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Frederik Barbre on 09/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        MapView()
+            .frame(height: 300)
+
+        CircleImage()
+            .offset(y: -130)
+            .padding(.bottom, -130)
+
+        VStack(alignment: .leading) {
+            Text("Turtle Neck")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park").font(.subheadline)
+                Spacer()
+                Text("California").font(.subheadline)
+            }
+
+            Divider()
+
+            Text("About Turtle Rock")
+                .font(.title2)
+            Text("Descriptive text goes here.")
+        }
+        .padding()
+
+        Spacer()
+    }
+}
+
+#Preview {
+    ContentView()
+}
