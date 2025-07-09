a70f0650c7316b56594448e660cbe4832aa69f7e
Frederik Barbre <fr.barbre@gmail.com>
2025-07-09 20:19:24 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Frederik Barbre on 09/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
